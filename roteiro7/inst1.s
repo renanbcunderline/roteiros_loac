c908be56512d94519cf926d4445500c145290cc6
Renan <100156618+renanbcunderline@users.noreply.github.com>
2022-12-13 11:51:29 -0300

    Create inst1.s

---
+++ roteiro7/inst1.s
@@ -0,0 +1,18 @@
+.text
+addi x12, x0, 1
+addi x10, x0, -2
+i: blt x10, x0, n
+beq x10, x0, z
+mul x12, x12, x10
+addi x10, x10, -1
+jal i
+
+z: addi  a0, x0, 1               
+add    a1, x0, x12               
+ecall
+n: addi  a0, x0, 4               
+la    a1, negativo               
+ecall
+
+.data                  
+negativo:  .asciiz "Fatorial inexistente\n"
