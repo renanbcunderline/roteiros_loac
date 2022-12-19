d1d98a9c91835b4a2ba668203c2c8abbbd585d77
Renan <100156618+renanbcunderline@users.noreply.github.com>
2022-12-18 21:17:29 -0300

    Update inst1.s

--- roteiro7/inst1.s
+++ roteiro7/inst1.s
@@ -1,6 +1,6 @@
 .text
 addi x12, x0, 1
-addi x10, x0, -2
+addi x10, x0, 3
 i: blt x10, x0, n
 beq x10, x0, z
 mul x12, x12, x10
@@ -8,11 +8,15 @@ addi x10, x10, -1
 jal i
 
 z: addi  a0, x0, 1               
-add    a1, x0, x12               
+add    a1, x0, x12
 ecall
+jal fim
+
 n: addi  a0, x0, 4               
-la    a1, negativo               
+la    a1, negativo 
 ecall
+
+fim: nop
 
 .data                  
 negativo:  .asciiz "Fatorial inexistente\n"
